ad073686bc514039b772ac38ea35e0139220c821
terraputix <terraputix@mailbox.org>
2025-12-08 14:20:21 +0100

    improve digits calculation

--- src/lib/components/scale/scale.svelte
+++ src/lib/components/scale/scale.svelte
@@ -19,6 +19,7 @@
 </script>
 
 {#if showScale}
+	{@const digits = Math.abs(Math.floor(1 / Math.log(colorScale.max - colorScale.min)))}
 	<div class="absolute bottom-2.5 left-2.5 z-10 max-h-[{colorScaleHeight + 100}px]">
 		<div
 			style="box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 0px 2px;"
@@ -29,7 +30,6 @@
 				style={mode.current === 'dark' ? 'background:black;' : 'background:white;'}
 			>
 				{#each colorScale.colors as cs, i (i)}
-					{@const digits = Math.floor(1 / Math.log(colorScale.max - colorScale.min))}
 					{@const value =
 						colorScale.min +
 						(i / (colorScale.colors.length - 1)) * (colorScale.max - colorScale.min)}
@@ -40,7 +40,6 @@
 				{/each}
 
 				{#each Array.from({ length: amountLabels + 1 }, (_, i) => (i * 100) / amountLabels) as step, i (i)}
-					{@const digits = Math.floor(1 / Math.log(colorScale.max - colorScale.min))}
 					{@const color = getColor(
 						colorScale,
 						Math.floor(
