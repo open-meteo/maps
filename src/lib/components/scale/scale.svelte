96f424763baafb0a187722acf3b5c0c720671b83
terraputix <terraputix@mailbox.org>
2025-12-10 14:17:45 +0100

    support color scales with breakpoints

--- src/lib/components/scale/scale.svelte
+++ src/lib/components/scale/scale.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { type RGBAColorScale, getColor, getColorScale } from '@openmeteo/mapbox-layer';
+	import { type RenderableColorScale, getColor, getColorScale } from '@openmeteo/mapbox-layer';
 	import { mode } from 'mode-watcher';
 
 	import { variable } from '$lib/stores/preferences';
@@ -12,15 +12,43 @@
 
 	let { showScale }: Props = $props();
 
-	const isDark = $derived(mode.current === 'dark');
-	const colorScale: RGBAColorScale = $derived(getColorScale($variable, isDark));
-
 	const colorScaleHeight = 500;
-	const amountLabels = 25;
+
+	const isDark = $derived(mode.current === 'dark');
+	const colorScale: RenderableColorScale = $derived(getColorScale($variable, isDark));
+
+	const getLabeledColorsForLegend = (colorScale: RenderableColorScale) => {
+		const labeledColors = [];
+		switch (colorScale.type) {
+			case 'rgba': {
+				const amountLabels = 25;
+				const step = (colorScale.max - colorScale.min) / amountLabels;
+
+				for (let i = 0; i <= amountLabels; i++) {
+					const value = Math.floor(colorScale.min + i * step);
+					const color = getColor(colorScale, value);
+					labeledColors.push({ value, color });
+				}
+				return labeledColors;
+			}
+			case 'breakpoint': {
+				for (let i = 0; i < colorScale.breakpoints.length; i++) {
+					const value = colorScale.breakpoints[i];
+					const color = colorScale.colors[i];
+					labeledColors.push({ value, color });
+				}
+				return labeledColors;
+			}
+		}
+	};
 </script>
 
 {#if showScale}
-	{@const digits = Math.abs(Math.floor(1 / Math.log(colorScale.max - colorScale.min)))}
+	{@const digits = 2}
+	{@const valueLength = String(Math.round(colorScale.max)).length}
+	{@const width = 17 + Math.max(valueLength, colorScale.unit.length + 1, digits + 2) * 4}
+	{@const labeledColors = getLabeledColorsForLegend(colorScale)}
+	{@const amountLabels = labeledColors.length}
 	<div class="absolute bottom-2.5 left-2.5 z-10 max-h-[{colorScaleHeight + 100}px]">
 		<div
 			style="box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 0px 2px;"
@@ -30,23 +58,21 @@
 				class="flex max-h-[{colorScaleHeight}px] flex-col-reverse"
 				style={isDark ? 'background:black;' : 'background:white;'}
 			>
-				{#each colorScale.colors as cs, i (i)}
-					{@const valueLength = String(Math.round(colorScale.max)).length}
-					{@const width = 17 + Math.max(valueLength, colorScale.unit.length + 1, digits + 2) * 4}
+				{#each labeledColors as lc, i (lc)}
 					<div
-						style={`background: rgba(${cs.join(',')});min-width: 28px; width: ${width}px; height: ${colorScaleHeight / colorScale.colors.length}px;`}
+						style={`background: rgba(${lc.color.join(',')});min-width: 28px; width: ${width}px; height: ${colorScaleHeight / amountLabels}px;`}
 					></div>
-				{/each}
-
-				{#each Array.from({ length: amountLabels + 1 }, (_, i) => i / amountLabels) as step, i (i)}
-					{@const value = Math.floor(colorScale.min + step * (colorScale.max - colorScale.min))}
-					{@const color = getColor(colorScale, value)}
-
 					<div
 						class="absolute w-full text-center text-xs"
-						style={`bottom:  ${2 + (colorScaleHeight * step * amountLabels) / (amountLabels + 1)}px; color: ${textWhite(color, isDark) ? 'white;' : 'black'}`}
+						style={`bottom:  ${2 + (colorScaleHeight / amountLabels) * i}px; color: ${textWhite(lc.color, isDark) ? 'white;' : 'black'}`}
 					>
-						{value.toFixed(digits)}
+						{#if lc.value >= 1 || lc.value <= -1}
+							{lc.value.toFixed(0)}
+						{:else if lc.value >= 0.1 || lc.value <= -0.1}
+							{lc.value.toFixed(1)}
+						{:else}
+							{lc.value.toFixed(digits)}
+						{/if}
 					</div>
 				{/each}
 			</div>
