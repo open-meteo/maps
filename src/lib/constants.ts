6eb3be8081d3a8940df6ffd4e5d801a719ac4b74
Vincent van der Wal <info@vincentvanderwal.nl>
2025-12-11 17:49:26 +0100

    add regex exception for min max anomaly

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,9 @@
 export const LEVEL_REGEX =
-	/(?<height_level_to>\d+_to_.*)|(?<pressure_level>\d+hPa)|(?<height_level>\d+m|cm)/;
+	/((?<height_level_to>\d+_to_.*)|(?<pressure_level>\d+hPa)|(?<height_level>\d+(m|cm)))(?!_)/;
 
 export const LEVEL_PREFIX =
 	/(?<prefix>(cloud_cover|geopotential_height|relative_humidity|temperature|soil_moisture|soil_temperature|temperature|vertical_velocity|wind(?!_gusts|_direction)))_/;
+
 export const LEVEL_UNIT_REGEX = /_(?<level>\d+)(?<unit>(m|cm|hPa))/;
 
 export const VARIABLE_PREFIX =
