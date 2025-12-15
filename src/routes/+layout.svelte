5a644ec2a1e22ee4672cb436734f84a6355c6f4f
Vincent van der Wal <info@vincentvanderwal.nl>
2025-12-15 13:04:21 +0100

    working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 
-	import { preferences } from '$lib/stores/preferences';
-
 	import { Toaster } from '$lib/components/ui/sonner';
 
 	let { children } = $props();
-
-	let timeSelectorOpen = $derived($preferences.timeSelector);
 </script>
 
 <Toaster
 	closeButton={true}
 	richColors={true}
-	offset={timeSelectorOpen ? { bottom: '130px', right: '10px' } : { bottom: '10px', right: '10px' }}
+	offset={false ? { bottom: '130px', right: '10px' } : { bottom: '10px', right: '10px' }}
 />
 
 {@render children()}
