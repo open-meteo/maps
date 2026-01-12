1d7535451a10eec41e84578ac494b9bd266bf1cc
Terraputix <terraputix@mailbox.org>
2026-01-12 11:33:00 +0100

    feat: Automatic partial requests (#213)

--- src/lib/stores/map.ts
+++ src/lib/stores/map.ts
@@ -1,10 +1,3 @@
 import { type Writable, writable } from 'svelte/store';
 
 export const map: Writable<maplibregl.Map> = writable();
-
-export const mapBounds: Writable<maplibregl.LngLatBounds | null> = writable(null);
-export const paddedBounds: Writable<maplibregl.LngLatBounds | null> = writable(null);
-export const paddedBoundsLayer: Writable<maplibregl.StyleLayer | undefined> = writable(undefined);
-export const paddedBoundsSource: Writable<maplibregl.GeoJSONSource | undefined> =
-	writable(undefined);
-export const paddedBoundsGeoJSON: Writable<GeoJSON.GeoJSON | null> = writable(null);
