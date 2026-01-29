dc2a8279e3288c7f55e6d7e06caaec8962e02fda
Vincent van der Wal <info@vincentvanderwal.nl>
2026-01-29 15:29:15 +0100

    prettier & eslint

--- src/lib/components/ui/sonner/sonner.svelte
+++ src/lib/components/ui/sonner/sonner.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import CircleCheckIcon from "@lucide/svelte/icons/circle-check";
-	import InfoIcon from "@lucide/svelte/icons/info";
-	import Loader2Icon from "@lucide/svelte/icons/loader-2";
-	import OctagonXIcon from "@lucide/svelte/icons/octagon-x";
-	import TriangleAlertIcon from "@lucide/svelte/icons/triangle-alert";
-
-	import { Toaster as Sonner, type ToasterProps as SonnerProps } from "svelte-sonner";
-	import { mode } from "mode-watcher";
+	import CircleCheckIcon from '@lucide/svelte/icons/circle-check';
+	import InfoIcon from '@lucide/svelte/icons/info';
+	import Loader2Icon from '@lucide/svelte/icons/loader-2';
+	import OctagonXIcon from '@lucide/svelte/icons/octagon-x';
+	import TriangleAlertIcon from '@lucide/svelte/icons/triangle-alert';
+	import { mode } from 'mode-watcher';
+	import { Toaster as Sonner, type ToasterProps as SonnerProps } from 'svelte-sonner';
 
 	let { ...restProps }: SonnerProps = $props();
 </script>
