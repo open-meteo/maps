bd961d58095809ad352bd0e1a7c3de19d2e8b441
Vincent van der Wal <111272374+vincentvdwal@users.noreply.github.com>
2026-07-17 13:53:26 +0200

    fix: overlapping attribution (#354)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,6 +45,7 @@
 	import Settings from '$lib/components/settings/settings.svelte';
 	import TimeSelector from '$lib/components/time/time-selector.svelte';
 
+	import { unwatchAttributionOverlap, watchAttributionOverlap } from '$lib/attribution';
 	import { checkHighDefinition } from '$lib/helpers';
 	import { addOmFileLayers, changeOMfileURL } from '$lib/layers';
 	import { addTerrainSource, getStyle, setMapControlSettings } from '$lib/map-controls';
@@ -133,6 +134,8 @@
 			addOmFileLayers();
 			addPopup();
 			changeOMfileURL();
+
+			watchAttributionOverlap();
 		});
 	});
 
@@ -189,6 +192,7 @@
 	});
 
 	onDestroy(() => {
+		unwatchAttributionOverlap();
 		if ($map) {
 			$map.remove();
 		}
